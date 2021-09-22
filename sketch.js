c333f931bbca63e5d33f8cf5f2fab491bd02337a
Cody <cody.tian@icloud.com>
2021-09-22 12:10:36 -0700

    add template project

---
+++ sketch.js
@@ -0,0 +1,25 @@
+/*
+@author cody
+@date 2021-09-22
+
+p5-bezier, based on videos on the Bezier curve from Daniel Shiffman and
+ Freya Holmér
+ */
+
+let font
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf');
+}
+
+function setup() {
+    createCanvas(600, 300)
+    colorMode(HSB, 360, 100, 100, 100)
+    background(0, 0, 30)
+}
+
+function draw() {
+    background(0, 0, 30)
+
+}
+
